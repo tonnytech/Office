3c0cb8e07bc98d0235807a79f08efc222e0ed878
tonnytech <tonnytei4@gmail.com>
2023-11-20 16:06:59 +0300

    set up react and create the hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navbar from './Navbar'
+import './Hero.css';
+
+const Hero = ()=> {
+  return (
+    <section className='heroMainContainer'>
+        <Navbar />
+        <div className='heroIntroContainer'>
+            <div className='heroIntro'>
+                <div className='heroIntroHeader'>
+                    <h3>Meet your</h3>
+                    <h2>New Office</h2>
+                </div>
+                <div className='heroParagraph'>
+                    <p>
+                    Elevate your work with our premium 
+                    co-working space—inspiring, flexible, 
+                    and collaborative. Join us today! 
+                    </p>
+                </div>
+                <div className='heroButtonContainer'>
+                    <button>
+                        Contact us
+                    </button>
+                    <button>
+                        Book a tour
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+import menu from '../Assets/menu.png';
+import office from '../Assets/office.png';
+import close from '../Assets/hamburgerclose.png';
+import './Navbar.css'
+
+const Navbar =()=> {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const ToggleNav =()=> {
+        setIsOpen(true);
+    }
+
+    const closeNav =()=> {
+        setIsOpen(false);
+    }
+  return (
+    <div className='NavbarMainContainer'>
+        <div className={isOpen ? 'hideMenu' : 'showMenu'}>
+            <img src={menu} className='hamburger' onClick={ToggleNav}/>
+        </div>
+
+        <div className='desktopLogo'>
+            <img src={office} className='hamburger'/>
+        </div>
+
+        <div className={isOpen ? 'open' : 'close'}>
+            
+            <ul>
+                <li>
+                    <img src={office} className='hamburger'/>
+                </li>
+                <li>menu</li>
+                <li>menu</li>
+                <li>menu</li>
+            </ul>
+            <div>
+                <img src={close} className='hamburger'onClick={closeNav}/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
